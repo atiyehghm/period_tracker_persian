cd4b9b0182100810d4abf8fe8c0a518c260c9319
‫‪ce441-981-94105785 <fahimeh94tolouee@gmail.com>
2020-08-04 02:17:31 +0430

    fix buttom navigation style

--- app/src/main/java/edu/sharif/periodtracker/MainActivity.java
+++ app/src/main/java/edu/sharif/periodtracker/MainActivity.java
@@ -1,34 +1,76 @@
 package edu.sharif.periodtracker;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import edu.sharif.periodtracker.ui.calendar.CalendarFragment;
+import edu.sharif.periodtracker.ui.content.ContentFragment;
+import edu.sharif.periodtracker.ui.home.HomeFragment;
+import edu.sharif.periodtracker.ui.report.ReportFragment;
 
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_calendar,R.id.navigation_add,
-                R.id.navigation_report, R.id.navigation_content)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        loadFragment(new HomeFragment(), R.string.title_home);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                int title;
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_content:
+                        fragment = new ContentFragment();
+                        title = R.string.title_content;
+                        break;
+                    case R.id.navigation_report:
+                        fragment = new ReportFragment();
+                        title = R.string.title_report;
+                        break;
+                    case R.id.navigation_calendar:
+                        fragment = new CalendarFragment();
+                        title = R.string.title_calendar;
+                        break;
+                    default:
+                        fragment = new HomeFragment();
+                        title = R.string.title_home;
+                        break;
+                }
+                return loadFragment(fragment, title);
+            }
+        });
+    }
+    private boolean loadFragment(Fragment fragment, int title) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.nav_host_fragment, fragment)
+                    .commit();
+            toolbar.setTitle(title);
+            return true;
+        }
+        return false;
     }
 
 }
