c8e5278ffbf452ace7186346ff3b0d39444563f3
atiyehghm <atiyeh1997@gmail.com>
2020-08-05 11:42:40 +0430

    commiting changes

---
+++ app/src/main/java/edu/sharif/periodtracker/ui/content/Article.java
@@ -0,0 +1,4 @@
+package edu.sharif.periodtracker.ui.content;
+
+public class Article {
+}
---
+++ app/src/main/java/edu/sharif/periodtracker/ui/content/ContentActivity.java
@@ -0,0 +1,6 @@
+package edu.sharif.periodtracker.ui.content;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContentActivity extends AppCompatActivity {
+}
