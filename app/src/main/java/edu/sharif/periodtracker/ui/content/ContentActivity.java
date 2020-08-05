2097776853c104779bdcf0ca6e89a3d39c5372ad
atiyehghm <atiyeh1997@gmail.com>
2020-08-05 11:46:57 +0430

    adding article parts

--- app/src/main/java/edu/sharif/periodtracker/ui/content/ContentActivity.java
+++ app/src/main/java/edu/sharif/periodtracker/ui/content/ContentActivity.java
@@ -1,6 +1,25 @@
 package edu.sharif.periodtracker.ui.content;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import edu.sharif.periodtracker.R;
+
 public class ContentActivity extends AppCompatActivity {
+    private TextView contentTitle;
+    private Article article;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        article = (Article)intent.getSerializableExtra("article");
+        setContentView(R.layout.activity_content);
+        contentTitle = findViewById(R.id.articleTitle);
+        contentTitle.setText(article.getTitle());
+        super.onCreate(savedInstanceState);
+    }
 }
